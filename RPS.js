9df1a0747065fe9c2c4d5de8f035926b21f67a89
Ramon Marcu <ramondotmarcu@gmail.com>
2021-04-15 16:55:56 +0300

    Latest Version

--- RPS.js
+++ RPS.js
@@ -1,18 +1,17 @@
 // rockPaperScissors.js
 
-const prompt = require('prompt-sync')();
- 
-const userInput = prompt('What is your choice?');
-console.log(userInput);
+const prompt = require('prompt-sync')({sigint: true});
 
-const getUserChoice = userInput => {
-userInput = userInput.toLowerCase();
+
+const getUserChoice = userInput => { userInput = userInput.toLowerCase();
 if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' || userInput === 'rocket') {
   return userInput;
  } else {
 console.log('Error! Please type: rock, paper or scissors.');
  }
-}
+};
+
+const userInput = prompt('Welcome! 👋  What is your choice?  Rock 🧱 , Paper 📄 or Scissors ✂️  ?');
 
 const getComputerChoice = () => {
  const randomNumber = Math.floor(Math.random() * 3)
@@ -27,38 +26,40 @@ const getComputerChoice = () => {
 };
 
 
+
 const determineWinner = (userChoice, computerChoice) => {
   if (userChoice === computerChoice) {
-    return 'This game is a tie!';
+    return '👾 This game is a tie! 👾 ';
   }
   if (userChoice === 'rock') {
     if (computerChoice === 'paper') {
-      return 'Sorry, computer won!';
+      return " 🤖 Sorry, computer won! 🤖 ";
     } else { 
-      return "Congratulations! You won!";
+      return "Congratulations! 🎉 You won! 🎉 ";
     }
   }
 
 if (userChoice === 'paper') {
   if(computerChoice === 'scissors') {
-    return "Sorry, computer won!";
+    return " 🤖 Sorry, computer won! 🤖 ";
   } else {
-    return "Congratulations! You won!";
+    return "Congratulations! 🎉 You won! 🎉 ";
   }
 }
 
 if (userChoice === 'scissors') {
   if(computerChoice === 'rock') {
-    return "Sorry, computer won!";
+    return " 🤖 Sorry, computer won! 🤖 ";
   } else {
-    return "Congratulations! You won!";
+    return "Congratulations! 🎉 You won! 🎉 ";
   }
  }
 
 if(userChoice === 'rocket') {
-  return 'Congratulations! You won with the secret cheat code!';
+  return 'Congratulations! 🎉 You won with the secret cheat code! 🚀 ';
 }
 };
+
 
 const playGame = () => {
   const userChoice = userInput;
@@ -73,16 +74,15 @@ const playGame = () => {
 playGame()
 
 
-/* Depending on what you choose and example of what console displays if running the code is:
 
-You threw: paper
-Computer threw: paper
-This game is a tie!
+/* If you download RPS.js and run it in terminal with: node RPS.js, the console will firstly display 3 raws:
 
-When you use the secret cheat code 'rocket' console will always display:
+You threw: (your choice)
+Computer threw: (computer's choice)
+(The decision)
 
-You threw: rocket
-Computer threw: rock
-Congratulations! You won with the secret cheat code! */
+By using the secret cheat code 'rocket' you'll always win against the computer's choice 
+
+To play it again you'll have to run it again with: node RPS.js */
 
 
